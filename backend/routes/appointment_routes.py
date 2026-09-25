6674297a781e9db8420ae2838128a4a94c2d6d1f
MathishaG <sudhadevi11ganesh@gmail.com>
2026-09-25 07:11:18 +0530

    Delete backend/routes/appointment_routes.py

--- backend/routes/appointment_routes.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, jsonify
-
-appointment_bp = Blueprint("appointment_bp", __name__)
-
-
-@appointment_bp.route("/", methods=["GET"])
-def get_appointments():
-    return jsonify({"message": "Appointment routes initialized"}), 200
