510c6b8cd9c4dd4d7679a8bb3d64ad070b6fcf02
MathishaG <sudhadevi11ganesh@gmail.com>
2026-09-25 07:11:50 +0530

    Delete backend/routes/student_routes.py

--- backend/routes/student_routes.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, jsonify
-
-student_bp = Blueprint("student_bp", __name__)
-
-
-@student_bp.route("/", methods=["GET"])
-def get_students():
-    return jsonify({"message": "Student routes initialized"}), 200
