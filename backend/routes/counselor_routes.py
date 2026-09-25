1d19c5810e5621dc18ac57ef4f035aed5df18e60
MathishaG <sudhadevi11ganesh@gmail.com>
2026-09-25 07:11:29 +0530

    Delete backend/routes/counselor_routes.py

--- backend/routes/counselor_routes.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, jsonify
-
-counselor_bp = Blueprint("counselor_bp", __name__)
-
-
-@counselor_bp.route("/", methods=["GET"])
-def get_counselors():
-    return jsonify({"message": "Counselor routes initialized"}), 200
